438db0c5e8a687ac6f211789b3dbca8cc4c725e8
ljp-projects <lucasplumb@icloud.com>
2025-03-19 07:10:45 +1100

    initial commit

---
+++ build.mjs
@@ -0,0 +1,43 @@
+import { writeFile, readFile } from 'node:fs/promises';
+import markdownit from 'markdown-it'
+import hljs from 'highlight.js'
+
+const convertMarkdown = async (inputFile, outputFile) => {
+    const md = markdownit()
+    const result = md.render(await readFile(inputFile, "utf-8"), {
+        highlight: function (str, lang) {
+            console.log(lang)
+
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(str, { language: lang }).value;
+                } catch (__) {}
+            }
+
+            return ''; // use external default escaping
+        }
+    });
+
+    const template = `
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <style>
+        @media (prefers-color-scheme: dark) {
+            * {
+                --background: #20382D !important;
+            }
+        }
+    </style>
+</head>
+<body>
+${result.slice(null, -1)}
+</body>
+</html>
+`;
+
+    await writeFile(outputFile, template);
+}
+
+convertMarkdown(process.argv[2], process.argv[3]);
---
+++ terms_of_use.md
@@ -0,0 +1,25 @@
+# Kolloquy Terms Of Use
+
+**Last modified on 18/03/2025**
+
+If you want to use Kolloquy, you must read & agree to these terms of use. You will not be able to create an account if you do not accept these terms of use or prove that you have read these terms of use.
+
+## Definitions
+In this document, **you** refers to the user who needs to agree to this document.
+
+**We** or **us** refers to Kolloquy, the platform.
+
+## Proof of Comprehension Code
+To use Kolloquy, you must prove that you have read and comprehended these terms of use.
+
+The first digit of the proof-of-comprehension code (PoCC) is the minimum age requirement minus 9.
+
+The next digits are the day and month this document was last modified, including a leading zero if it is there.
+
+## Age Requirements
+To use Kolloquy, you must be 13 or older.
+
+## Privacy
+
+### How We Store Your Messages
+Your messages are stored as an end-to-end-encrypted (E2EE) log file containing every message that has been sent. **We cannot see the messages that you send**.
